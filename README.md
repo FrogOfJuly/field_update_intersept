f6a86357ec7835b811523f3b8cc1700e54fd3ae3
Kirill Golubev <frogofjuly@Me.local>
2024-01-27 16:29:20 +0300

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Field update interseption
+
+How to execute arbitrary code before each field update?
---
+++ src/fint.c
@@ -0,0 +1,6 @@
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
