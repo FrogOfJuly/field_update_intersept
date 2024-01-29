3b9d9d5e6bb3c87a1f39aa582b4334c698290a33
frogofjuly <golubev.ki.ig@gmail.com>
2024-01-29 17:00:46 +0000

    field updates

--- examples/field_update.py
+++ examples/field_update.py
@@ -1,4 +1,28 @@
 import field_intersept
 
 
+class Foo(object):
+    def __init__(self):
+        print("field updates in init")
+        x = 1
+        self.x = x
+        self.y = 2
+        self.z = self.y
+
+
+f = Foo()
+
+print("field updates outside")
+f.x = 3
+f.y = 4
+f.z = 5
+
+print(Foo.__bases__)
+
+print("field update settatr")
+print(f.x)
+object.__setattr__(f, "x", 1)
+print(f.x)
+
+
 field_intersept.stop()
