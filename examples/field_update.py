bd489e4c33317c4ff493ebcf669dae4bd88b4ae9
frogofjuly <golubev.ki.ig@gmail.com>
2024-02-06 20:02:04 +0000

    showing different field updates

--- examples/field_update.py
+++ examples/field_update.py
@@ -1,23 +1,25 @@
 import field_intersept
 
+print("- declaring class")
 
 class Foo(object):
     def __init__(self):
-        print("field updates in init")
+        print("- field updates in init")
         x = 1
         self.x = x
         self.y = 2
         self.z = self.y
 
+print("- init starts")
 
 f = Foo()
+
+print("- init finished")
 
 print("field updates outside")
 f.x = 3
 f.y = 4
 f.z = 5
-
-print(Foo.__bases__)
 
 print("field update settatr")
 print(f.x)
